59ccb4894d698f127503effb53445c108a8a6a26
Justin Kwan <jkwan19@gmail.com>
2021-02-27 17:28:37 -0800

    Implement sign in and sign out

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => (
@@ -18,7 +19,7 @@ const INITIAL_STATE = {
   error: null,
 };
 
-class SignUpForm extends Component {
+class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
 
@@ -26,8 +27,20 @@ class SignUpForm extends Component {
   }
 
   onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
 
-  }
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -93,6 +106,8 @@ const SignUpLink = () => (
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
+
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 export default SignUpPage;
 
